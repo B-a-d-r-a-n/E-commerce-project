dfbc765f3dd73162bdf4a711b9769832a9e69254
Mohamed Badran <41090487+B-a-d-r-a-n@users.noreply.github.com>
2024-09-08 13:58:05 +0300

    details updated

--- src/app/components/details/details.component.ts
+++ src/app/components/details/details.component.ts
@@ -2,6 +2,8 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { ProductsService } from '../../core/services/products.service';
 import { Product } from '../../core/interfaces/products';
+import { CartService } from '../../core/services/cart.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-details',
@@ -13,8 +15,21 @@ import { Product } from '../../core/interfaces/products';
 export class DetailsComponent implements OnInit {
   private readonly _ActivatedRoute = inject(ActivatedRoute);
   private readonly _ProductsService = inject(ProductsService);
+  private readonly _CartService = inject(CartService);
+  private readonly _toastr = inject(ToastrService);
+
   product!: Product;
 
+  addToCart = (productId: string) => {
+    this._CartService.addProductToCart(productId).subscribe({
+      next: (res) => {
+        this._toastr.success(`Product added successfully`, '', {
+          tapToDismiss: true,
+          timeOut: 2000,
+        });
+      },
+    });
+  };
   ngOnInit(): void {
     let id = '';
     this._ActivatedRoute.paramMap.subscribe({
@@ -22,9 +37,10 @@ export class DetailsComponent implements OnInit {
         id = param.get('id')!;
       },
     });
-
     this._ProductsService.getProduct(id).subscribe({
       next: (res) => {
+        console.log(res);
+
         this.product = res.data;
       },
     });
